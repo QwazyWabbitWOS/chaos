f2d9c64ed7e28f946f68dc9b7c4c695becf47ebf
QwazyWabbit <gandalf.2004@gmail.com>
2021-09-21 15:30:08 -0700

    Mindless re-tabbing of itemlist from VC 6.0 buggy formatter.

--- g_local.h
+++ g_local.h
@@ -203,29 +203,29 @@ typedef struct
 typedef struct gitem_s
 {
 	unsigned int	classindex; /* MrG{DRGN} added */
-	char* classname;	// spawning name
+	char*	classname;	// spawning name
 	qboolean(*pickup)(struct edict_s* ent, struct edict_s* other);
-	void		(*use)(struct edict_s* ent, struct gitem_s* item);
-	void		(*drop)(struct edict_s* ent, struct gitem_s* item);
-	void		(*weaponthink)(struct edict_s* ent);
-	char* pickup_sound;
-	char* world_model;
-	int			world_model_flags;
-	char* view_model;
+	void	(*use)(struct edict_s* ent, struct gitem_s* item);
+	void	(*drop)(struct edict_s* ent, struct gitem_s* item);
+	void	(*weaponthink)(struct edict_s* ent);
+	char*	pickup_sound;
+	char*	world_model;
+	int		world_model_flags;
+	char*	view_model;
 
 	// client side info
-	char* icon;
-	char* pickup_name;	// for printing on pickup
-	int			count_width;		// number of digits to display by icon
+	char*	icon;
+	char*	pickup_name;	// for printing on pickup
+	int		count_width;		// number of digits to display by icon
 
-	int			quantity;		// for ammo how much, for weapons how much is used per shot
-	char* ammo;			// for weapons
-	int			flags;			// IT_* flags
+	int		quantity;		// for ammo how much, for weapons how much is used per shot
+	char*	ammo;			// for weapons
+	int		flags;			// IT_* flags
 
-	void* info;
-	int			tag;
+	void*	info;
+	int		tag;
 
-	char* precaches;		// string of all models, sounds, and images this item will use
+	char*	precaches;		// string of all models, sounds, and images this item will use
 } gitem_t;
 
 //
