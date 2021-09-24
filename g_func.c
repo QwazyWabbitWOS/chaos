ba8db616ec29c70f6cde09bcb3269800965d2f69
MrG-DRGN <imfriend@hotmail.com>
2021-09-24 12:15:43 -0400

    g_func.c: Removed unneeeded sanity check in door_use.

--- g_func.c
+++ g_func.c
@@ -1000,12 +1000,6 @@ void door_use(edict_t* self, edict_t* other, edict_t* activator)
 {
 	edict_t* ent;
 
-	/* MrG{DRGN} sanity check */
-	if (!self)
-	{
-		return;
-	}
-
 	if (self->flags & FL_TEAMSLAVE)
 		return;
 
