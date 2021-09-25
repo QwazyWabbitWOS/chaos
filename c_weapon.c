d5029e5bcee5fb52707526009412a6364de8ca51
QwazyWabbit <gandalf.2004@gmail.com>
2021-09-24 20:58:41 -0700

    Avoid dereferencing null pointer.

--- c_weapon.c
+++ c_weapon.c
@@ -539,7 +539,7 @@ void fire_lead3(edict_t* self, vec3_t start, vec3_t aimdir, int damage, int kick
 			}
 			else
 			{
-				if (strncmp(tr.surface->name, "sky", 3) != 0)
+				if (tr.surface && strncmp(tr.surface->name, "sky", 3) != 0)
 				{
 					gi.WriteByte(svc_temp_entity);
 					gi.WriteByte(te_impact);
@@ -1862,7 +1862,7 @@ void fire_lead2(edict_t* self, vec3_t start, vec3_t aimdir, int damage, int kick
 			}
 			else
 			{
-				if (strncmp(tr.surface->name, "sky", 3) != 0)
+				if (tr.surface && strncmp(tr.surface->name, "sky", 3) != 0)
 				{
 					if (self->client)
 						PlayerNoise(self, tr.endpos, PNOISE_IMPACT);
