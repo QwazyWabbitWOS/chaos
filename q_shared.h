03930da7ad75150b3c46ddb4ef7f08bb635242df
QwazyWabbit <gandalf.2004@gmail.com>
2021-10-05 18:14:28 -0700

    Newlines at end of files and eliminate reference to _MSC_VER

--- q_shared.h
+++ q_shared.h
@@ -28,12 +28,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #pragma warning(disable : 4305) // truncation from double to float
 #pragma warning(disable : 4244)	// C4244 conversion from 'type1' to 'type2', possible loss of data
 #pragma warning(disable : 4100)	// C4100 unreferenced formal parameter
-#pragma warning(disable : 4459)    // declaration of 'var' hides global declaration
-
-#if _MSC_VER > 1500
+#pragma warning(disable : 4459) // declaration of 'var' hides global declaration
 #pragma warning(disable : 4996)	// disable warnings from VS 2010 about deprecated CRT functions (_CRT_SECURE_NO_WARNINGS).
-#endif /*_MSC_VER */
-#endif /* _WIN32 */
+#endif
 
 #include <assert.h>
 #include <errno.h>
