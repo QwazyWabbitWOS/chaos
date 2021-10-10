50ef083dfd73f9f3f95cbd1547f7a37eeb1c792a
MrG-DRGN <imfriend@hotmail.com>
2021-10-10 18:00:42 -0400

    Files eliminated from project.

--- b_log.h
+++
@@ -1,9 +0,0 @@
-/*
-***************
-* b_log.c *
-***************
-*/
-void Log_Printf(const char* format, ...);
-void Log_PrintfNoDate(const char* format, ...);
-void Log_PrintfWithLocation(const char* File, int Line, const char* msg);
-#define LOGPRINTF(QQ) Log_Printf("%s(%d): %s", __FILE__, __LINE__, QQ)
