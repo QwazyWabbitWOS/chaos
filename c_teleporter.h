784f9bb77d4006c607236c53c6d6e21fde7599ee
MrG-DRGN <imfriend@hotmail.com>
2021-10-28 12:45:05 -0400

    Remember the prototype!

--- c_teleporter.h
+++ c_teleporter.h
@@ -1,6 +1,8 @@
 #ifndef C_TELEPORTER_H
 #define C_TELEPORTER_H
 
+void Shutoff_teleporter(edict_t* ent);
+
 // MrG{DRGN} never in game, but always posessed!
 gitem_t* it_teleporter;
 
@@ -9,6 +11,8 @@ void Teleport(edict_t* ent);
 void Cmd_Teleport_f(edict_t* ent);
 
 #endif
+
+
 
 
 
