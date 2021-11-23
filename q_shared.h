5cea17c0fc0aaf11626018783ab8231ddfdb37d3
MrG-DRGN <imfriend@hotmail.com>
2021-11-23 03:28:44 -0500

    removed duplicate include of math.h

--- q_shared.h
+++ q_shared.h
@@ -38,7 +38,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
-#include <math.h>
 #include <ctype.h>
 
 typedef unsigned char 		byte;
