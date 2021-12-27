60adbd930aaa5c33294b1da24eb4f88769b80f67
MrG-DRGN <imfriend@hotmail.com>
2021-12-27 09:32:52 -0500

    Add extern __inline int Q_tolower(int c); in q_shared.h

--- q_shared.h
+++ q_shared.h
@@ -194,7 +194,7 @@ void Com_PageInMemory(byte* buffer, int size);
 
 //=============================================
 
-int Q_tolower(int c);
+extern __inline int Q_tolower(int c);
 // portable case insensitive compare
 int Q_stricmp(const char* s1, const char* s2);
 int Q_strnicmp(const char* s1, const char* s2, size_t count);
